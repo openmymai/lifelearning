70bda2c93e930a9b7a9982f5efb5decee006db2f
openmymai <maicmi@me.com>
2023-02-13 14:48:12 +0700

    Enable vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
-import Script from 'next/script';
 import Layout from '../components/Layout';
+import { Analytics } from '@vercel/analytics/react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/style.css';
 import '@fortawesome/fontawesome-free/css/all.css';
@@ -8,6 +8,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'boxicons/css/boxicons.min.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+
 
 
 const MyApp = ({ Component, pageProps }) => {
@@ -18,6 +19,7 @@ const MyApp = ({ Component, pageProps }) => {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 };
