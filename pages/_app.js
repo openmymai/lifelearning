95da2de4f22d3f98e968d300525421f5ac2cb2cd
openmymai <maicmi@me.com>
2023-02-25 13:46:55 +0700

    Enable google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
 import { Analytics } from '@vercel/analytics/react';
 import Layout from '../components/Layout';
 import 'bootstrap-icons/font/bootstrap-icons.css';
@@ -10,15 +12,49 @@ import 'aos/dist/aos.css';
 
 
 const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+
   useEffect(() => {
     AOS.init();
   },[]);
 
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return ( 
-    <Layout>
-      <Component {...pageProps} />
-      <Analytics />
-    </Layout>
+    <>
+      <Head>
+        <title>Life Learning</title>
+        <script 
+          async 
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+              __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+          `,
+          }}
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+        <Analytics />
+      </Layout>
+    </>
   )
 };
 
