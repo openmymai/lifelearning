fc79ccbe2648511733a2cb3274922b653ec48e55
openmymai <maicmi@me.com>
2023-10-07 20:14:16 +0700

    Update Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
@@ -6,50 +6,91 @@ const Footer = () => {
       <div className="footer-top">
         <div className="container">
           <div className="row">
-
             <div className="col-lg-6">
               <h3>พระพุทธเจ้าสอนอะไร?</h3>
-              <h5><a href="/buddhateaching/life01">
-                ปัญญา ศีล สมาธิ<br />
-                เป็นคำสอนของพระพุทธเจ้า<br />
-                นำไปปฏิบัติอย่างไร?<br /><br />
-                </a>
-              </h5>
               <h5>
-                <a href="https://youtube.com/playlist?list=PLkszHNgmI1k9johLnMfvs-7QMcLocNGWA">
-                เสียงอ่านพระไตรปิฎก
+                <a href="/buddhateaching/life01">
+                  ปัญญา ศีล สมาธิ
+                  <br />
+                  เป็นคำสอนของพระพุทธเจ้า
+                  <br />
+                  นำไปปฏิบัติอย่างไร?
+                  <br />
+                  <br />
                 </a>
               </h5>
               <h6>
-                เชียงใหม่, Chiang Mai<br />
-                ประเทศไทย, Thailand<br /><br />
+                เชียงใหม่, Chiang Mai
+                <br />
+                ประเทศไทย, Thailand
+                <br />
+                <br />
               </h6>
             </div>
 
             <div className="col-lg-6 footer-links">
               <h4>Useful Links</h4>
               <ul>
-                <li><i className="bi bi-chevron-right"></i><a className="nav-link" href="/#hero">หน้าแรก</a></li>
-                <li><i className="bi bi-chevron-right"></i><a className="nav-link" href="/#primary">บทวิปัสสนาภาวนา</a></li>
-                <li><i className="bi bi-chevron-right"></i><a className="nav-link" href="/#services">หลักสูตรอนาคามีผล</a></li>
-                <li><i className="bi bi-chevron-right"></i><a className="nav-link" href="/#additional">บทวิปัสสนาภาวนาเพิ่มเติม</a></li>
-                <li><i className="bi bi-chevron-right"></i><a className="nav-link" href="/#team">บทนำไปใช้ในชีวิตประจำวัน</a></li>
+                <li>
+                  <i className="bi bi-chevron-right"></i>
+                  <a
+                    className="nav-link"
+                    href="/#hero"
+                  >
+                    หน้าแรก
+                  </a>
+                </li>
+                <li>
+                  <i className="bi bi-chevron-right"></i>
+                  <a
+                    className="nav-link"
+                    href="/#primary"
+                  >
+                    บทวิปัสสนาภาวนา
+                  </a>
+                </li>
+                <li>
+                  <i className="bi bi-chevron-right"></i>
+                  <a
+                    className="nav-link"
+                    href="/#services"
+                  >
+                    หลักสูตรอนาคามีผล
+                  </a>
+                </li>
+                <li>
+                  <i className="bi bi-chevron-right"></i>
+                  <a
+                    className="nav-link"
+                    href="/#additional"
+                  >
+                    บทวิปัสสนาภาวนาเพิ่มเติม
+                  </a>
+                </li>
+                <li>
+                  <i className="bi bi-chevron-right"></i>
+                  <a
+                    className="nav-link"
+                    href="/#team"
+                  >
+                    บทนำไปใช้ในชีวิตประจำวัน
+                  </a>
+                </li>
               </ul>
             </div>
-
           </div>
         </div>
       </div>
 
       <div className="container footer-bottom clearfix">
         <div className="credits">
-          Designed by <a href="https://bootstrapmade.com/">Bootstrapmade</a> &nbsp;
-          Implement using <a href="https://nextjs.org/">NextJS</a>&nbsp; 
+          Designed by <a href="https://bootstrapmade.com/">Bootstrapmade</a>{" "}
+          &nbsp; Implement using <a href="https://nextjs.org/">NextJS</a>&nbsp;
           by <a href="https://openmymai.github.io">Sirisak Chantanate (Mai)</a>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
