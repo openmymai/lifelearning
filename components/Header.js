845bf41aa3a2720f14a678a1688afba90e8cf9dd
openmymai <maicmi@me.com>
2024-08-04 09:04:50 +0700

    Fix typo Lesson0511

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 const useWindowSize = () => {
-  const [ windowSize, setWindowSize ] = useState({
+  const [windowSize, setWindowSize] = useState({
     width: undefined,
     height: undefined,
   });
@@ -12,44 +12,95 @@ const useWindowSize = () => {
         width: window.innerWidth,
         height: window.innerHeight,
       });
-    }
-    window.addEventListener("resize", handleResize);
+    };
+    window.addEventListener('resize', handleResize);
 
     handleResize();
 
-    return () => window.removeEventListener("resize", handleResize);
-  },[]);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return windowSize;
-}
+};
 
 const Header = () => {
-  const [ navbarOpen, setNavbarOpen ] = useState(false);
-  
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
   const size = useWindowSize();
   const ref = useRef();
   return (
     <div>
       {!navbarOpen ? (
-        <i className="bi bi-list mobile-nav-toggle d-xl-none" onClick={() => setNavbarOpen((prev) => !prev)}></i>
+        <i
+          className='bi bi-list mobile-nav-toggle d-xl-none'
+          onClick={() => setNavbarOpen((prev) => !prev)}
+        ></i>
       ) : (
-        <i className="bi bi-x mobile-nav-toggle d-xl-none" onClick={() => setNavbarOpen((prev) => !prev)}></i>
+        <i
+          className='bi bi-x mobile-nav-toggle d-xl-none'
+          onClick={() => setNavbarOpen((prev) => !prev)}
+        ></i>
       )}
-      <header id="header">
-        <div className="container d-flex align-items-center">
-          <nav id="navbar" ref={ref} className={`${navbarOpen && size.width < 992 ? 'navbar navbar-mobile' : 'navbar'}`}>
+      <header id='header'>
+        <div className='container d-flex align-items-center'>
+          <nav
+            ref={ref}
+            className={`${
+              navbarOpen && size.width < 992 ? 'navbar navbar-mobile' : 'navbar'
+            }`}
+          >
             <ul>
-              <li><a onClick={() => setNavbarOpen((prev) => !prev)} className="nav-link" href="/#hero">หน้าแรก</a></li>
-              <li><a onClick={() => setNavbarOpen((prev) => !prev)} className="nav-link" href="/#primary">บทวิปัสสนาภาวนา</a></li>
-              <li><a onClick={() => setNavbarOpen((prev) => !prev)} className="nav-link" href="/#services">หลักสูตรอนาคามีผล</a></li>
-              <li><a onClick={() => setNavbarOpen((prev) => !prev)} className="nav-link" href="/#additional">บทวิปัสสนาภาวนาเพิ่มเติม</a></li>
-              <li><a onClick={() => setNavbarOpen((prev) => !prev)} className="nav-link" href="/#team">บทนำไปใช้ในชีวิตประจำวัน</a></li>
+              <li>
+                <a
+                  onClick={() => setNavbarOpen((prev) => !prev)}
+                  className='nav-link'
+                  href='/#hero'
+                >
+                  หน้าแรก
+                </a>
+              </li>
+              <li>
+                <a
+                  onClick={() => setNavbarOpen((prev) => !prev)}
+                  className='nav-link'
+                  href='/#primary'
+                >
+                  บทวิปัสสนาภาวนา
+                </a>
+              </li>
+              <li>
+                <a
+                  onClick={() => setNavbarOpen((prev) => !prev)}
+                  className='nav-link'
+                  href='/#services'
+                >
+                  หลักสูตรอนาคามีผล
+                </a>
+              </li>
+              <li>
+                <a
+                  onClick={() => setNavbarOpen((prev) => !prev)}
+                  className='nav-link'
+                  href='/#additional'
+                >
+                  บทวิปัสสนาภาวนาเพิ่มเติม
+                </a>
+              </li>
+              <li>
+                <a
+                  onClick={() => setNavbarOpen((prev) => !prev)}
+                  className='nav-link'
+                  href='/#team'
+                >
+                  บทนำไปใช้ในชีวิตประจำวัน
+                </a>
+              </li>
             </ul>
           </nav>
         </div>
       </header>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
